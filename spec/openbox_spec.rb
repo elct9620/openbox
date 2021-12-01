0945dfbba6c16942d9f1198d92940a69f642fc97
蒼時弦也 <elct9620@frost.tw>
2021-12-01 16:01:57 +0800

    Add Openbox.runtime and Openbox.database tests

--- spec/openbox_spec.rb
+++ spec/openbox_spec.rb
@@ -4,4 +4,16 @@ RSpec.describe Openbox do
   it 'has a version number' do
     expect(Openbox::VERSION).not_to be nil
   end
+
+  describe '.runtime' do
+    subject { described_class.runtime }
+
+    it { is_expected.to be_a(Openbox::Runtime) }
+  end
+
+  describe '.database' do
+    subject { described_class.database }
+
+    it { is_expected.to be_a(Openbox::Database) }
+  end
 end

5203b9993eb0f8d0ed262dd4ff0cebc51f873f48
蒼時弦也 <elct9620@frost.tw>
2021-12-01 13:51:21 +0800

    Runtime#select only pickup default environment

--- lib/openbox/runtime.rb
+++ lib/openbox/runtime.rb
@@ -7,17 +7,33 @@ module Openbox
   #
   # @since 0.1.0
   class Runtime
+    # Check for Rails environment
+    #
+    # @return [TrueClass|FalseClass]
+    #
+    # @since 0.1.0
     def rails?
       has?('rails')
     end
 
+    # Select desired rubygems
+    #
+    # @return [Array<Bundler::Dependency>]
+    #
+    # @since 0.1.0
     def select(*names)
+      # TODO: Select depend on RAILS_ENV or RACK_ENV
       Bundler
         .definition
-        .dependencies
-        .select { |dep| names.include?(dep.name) }
+        .current_dependencies
+        .select { |dep| names.include?(dep.name) && dep.groups.include?(:default) }
     end
 
+    # Check for rubygmes exists
+    #
+    # @return [TrueClass|FalseClass]
+    #
+    # @since 0.1.0
     def has?(*names)
       select(*names).any?
     end
